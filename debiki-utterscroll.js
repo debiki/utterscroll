c3f9de9c3f29797b412014053d2fb59e978a0035
Kaj Magnus Lindberg <kajmagnus79@gmail.com>
2012-12-24 13:41:34 +0100

    Use copyright and license notice suggested by www.gnu.org/.

--- debiki-utterscroll.js
+++ debiki-utterscroll.js
@@ -1,11 +1,20 @@
-/**
- * Debiki Utterscroll — dragscroll everywhere
+/* Debiki Utterscroll — dragscroll everywhere
+ * http://www.debiki.com/dev/utterscroll
  *
- * Copyright (c) 2012 Kaj Magnus Lindberg
+ * Copyright (c) 2012 Kaj Magnus Lindberg (born 1979)
  *
- * Licensed under the GNU Lesser General Public License version 3,
- * or any later version:
- *   http://www.gnu.org/licenses/lgpl.txt
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, see <http://www.gnu.org/licenses/>.
  */
 
 //----------------------------------------
